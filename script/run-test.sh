73e6ec9430ceaf0f159283be5d523823df299e96
Cristian Nieto <cristian.nieto@mercadolibre.com>
2016-05-30 15:47:38 -0300

    test 2

--- script/run-test.sh
+++ script/run-test.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-xcodebuild \
-	-project Coveralls-iOS.xcodeproj \
-	-sdk iphonesimulator \
-	-target Coveralls-iOSTests \
-	-configuration Debug \
-	clean build \
-	ONLY_ACTIVE_ARCH=NO \
-	TEST_AFTER_BUILD=YES \
-	GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
-	GCC_GENERATE_TEST_COVERAGE_FILES=YES 
+set -ev
+
+cd $TRAVIS_BUILD_DIR/
+xctool test ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
